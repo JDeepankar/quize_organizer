08550d78bd3c3a761b45f1ac9e069e3f83817cb0
Deepankar <deepankar.4net@gmail.com>
2025-12-09 11:39:43 +0530

    Initial structure of spring boot application - quize_organiser

---
+++ quiz-organiser/src/main/java/com/app/quiz_organiser/QuizOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.app.quiz_organiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizOrganiserApplication.class, args);
+	}
+
+}
---
+++ quiz-organiser/src/test/java/com/app/quiz_organiser/QuizOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.quiz_organiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizOrganiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
